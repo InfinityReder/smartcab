1d81ce9fc7ad1515e7f43891bdc0ba8f8ae5dc7b
IRR <irr@IRRdeMacBook-Pro.local>
2017-08-23 23:05:44 +0800

    fix

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -195,7 +195,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning = True, epsilon = 1, alpha = .5)
+    agent = env.create_agent(LearningAgent, learning = True, epsilon = 1, alpha = .6)
     
     ##############
     # Follow the driving agent
@@ -217,7 +217,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run( n_test = 20 , tolerance = .27)
+    sim.run( n_test = 20 , tolerance = .1)
 
 
 if __name__ == '__main__':
